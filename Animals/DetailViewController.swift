3fb77a8640f27ecc9586a9f97152be0dddad9c91
mzgk <mzgkworks@gmail.com>
2018-01-29 16:14:12 +0900

    add: 詳細画面のVCカスタムクラスを設定

---
+++ Animals/DetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  DetailViewController.swift
+//  Animals
+//
+//  Created by 水垣岳志 on 2018/01/29.
+//  Copyright © 2018年 mzgkworks.com. All rights reserved.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
